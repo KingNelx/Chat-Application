15d7366de4a711dbac6d989f226278798d8c9eda
KingNelx <tapiajonel606@gmail.com>
2023-05-02 20:19:54 +0800

    created a join rom component

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import io from 'socket.io-client'
+import JoinRoom from './Components/JoinRoom';
 
 const socket = io.connect("http://localhost:9000")
 
 const App = () => {
   return ( 
     <div>
-      
+     
+        <JoinRoom />
+  
     </div>
    );
 }
---
+++ client/client/src/Components/JoinRoom.jsx
@@ -0,0 +1,26 @@
+const JoinRoom = () => {
+    return (
+      <div className="container" style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <div class="card" style={{ width: "18rem" }}>
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">JOIN CHAT</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default JoinRoom;
+  
