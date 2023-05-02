2e2ac91b1d2f4fe91925e81bff9749d04e565d06
KingNelx <tapiajonel606@gmail.com>
2023-05-02 20:34:10 +0800

    added use state for conditional checking

--- client/client/src/Components/JoinRoom.jsx
+++ client/client/src/Components/JoinRoom.jsx
@@ -1,26 +1,49 @@
+import chatroom from './Images/chatroom.webp'
+import { useState } from 'react';
+
 const JoinRoom = () => {
+
+
+    const [userName, setUserName] = useState("")
+    const [room, setRoom] = useState("")
+    const joinRoom = () => {
+
+        if(userName !== "" && room !== ""){
+
+        }
+    }   
+
     return (
-      <div className="container" style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-        <div class="card" style={{ width: "18rem" }}>
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">JOIN CHAT</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
+        <div className="container" style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "15vh"
+        }}>
+            <div class="card shadow-lg" style={{ width: "18rem" }}>
+                <img src={chatroom} class="card-img-top" alt="..." />
+                <h5 class="card-title text-center">JOIN CHAT</h5>
+                <div class="card-body">
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">@</span>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"
+                            onChange={(e) => setUserName(e.target.value)} />
+                    </div>
+
+                    <div class="input-group flex-nowrap mt-2">
+                        <span class="input-group-text" id="addon-wrapping">@</span>
+                        <input type="text" class="form-control" placeholder="Room ID" aria-label="Username" aria-describedby="addon-wrapping"
+                            onChange={(e) => setRoom(e.target.value)} />
+                    </div>
+
+                    <div class="text-center mt-2">
+                        <button type="button" class="btn btn-outline-primary">JOIN</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default JoinRoom;
-  
+};
+
+export default JoinRoom;
