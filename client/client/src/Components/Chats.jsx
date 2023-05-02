f115865756532604e5c840da9514e7fee7718e3b
KingNelx <tapiajonel606@gmail.com>
2023-05-02 22:49:14 +0800

    finish

--- client/client/src/Components/Chats.jsx
+++ client/client/src/Components/Chats.jsx
@@ -1,33 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import avatar from './Images/avatar.webp'
+const Chats = ({ socket, username, room }) => {
 
-const Chats = ({ socketProp, username, room }) => {
-    return (
-        <div class="chat-container">
-            <div class="chat-header bg-light">
-                <h4 class="m-0">Chat Title</h4>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Leave Chat</button>
-            </div>
-            <div class="chat-body">
-                <div class="message">
-                    <div class="message-sender">
-                        <img src="https://via.placeholder.com/36" class="rounded-circle mr-2" /> Sender Name
-                        <span class="message-time float-right">12:34 PM</span>
-                    </div>
-                    <div class="message-content">Message text goes here.</div>
+
+    const [currentChat, setCurrentChat] = useState("")
+    const [messageList, setMessageList] = useState([]);
+    const sendMessage = async (event) => {
+        event.preventDefault();
+        if (currentChat !== "") {
+            const chatData = {
+                room: room,
+                author: username,
+                chat: currentChat,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+            await socket.emit("send_message", chatData)
+            setMessageList((list) => [...list, chatData]);
+        }
+    }
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            setMessageList((list) => [...list, data]);
+        });
+        return () => {
+            socket.off("receive_message");
+        };
+    }, [socket]);
+    
+
+      return (
+        <div className="chat-container">
+          <div className="chat-header bg-light d-flex justify-content-between align-items-center py-2 px-3">
+            <h4 className="m-0">Chat Title</h4>
+            <button type="button" className="btn btn-sm btn-outline-secondary">Leave Chat</button>
+          </div>
+          <div className="chat-body p-3">
+            {messageList.map((message, index) => (
+              <div key={index} className="message mb-3">
+                <div className="message-sender d-flex align-items-center mb-2">
+                <img src={avatar} className="rounded-circle mr-2" alt="Sender Avatar" style={{ width: '36px', height: '36px' }} />
+                  <span className="font-weight-bold mx-2">{ message.author }</span>
+                  <span className="message-time ml-auto">{ message.time }</span>
                 </div>
-
-            </div>
-            <div class="chat-footer">
-                <form class="form-inline">
-                    <div class="form-group flex-grow-1 mr-2">
-                        <input type="text" class="form-control w-100" placeholder="Type your message here..." />
-                    </div>
-                    <button type="submit" class="btn btn-primary">Send</button>
-                </form>
-            </div>
+                <div className="message-content">{message.chat}</div>
+              </div>
+            ))}
+          </div>
+          <div className="chat-footer p-3">
+            <form className="form-inline">
+              <div className="form-group flex-grow-1 mr-2">
+                <input type="text" className="form-control w-100" placeholder="Type your message here..." onChange={(e) => setCurrentChat(e.target.value)} />
+              </div>
+              <button className="btn btn-primary mt-3" onClick={sendMessage}>Send</button>
+            </form>
+          </div>
         </div>
-
-    );
+      );
+      
 }
 
 export default Chats;
--- client/client/src/Components/JoinRoom.jsx
+++ client/client/src/Components/JoinRoom.jsx
@@ -9,12 +9,15 @@ const JoinRoom = () => {
 
     const [userName, setUserName] = useState("")
     const [room, setRoom] = useState("")
+    const [showChat, setShowChat] = useState(false)
+
     const joinRoom = () => {
 
-        if(userName !== "" && room !== ""){
+        if (userName !== "" && room !== "") {
             socket.emit("JOIN_CHAT_ROOM", room)
+            setShowChat (true)
         }
-    }   
+    }
 
     return (
         <div className="container" style={{
@@ -23,31 +26,34 @@ const JoinRoom = () => {
             alignItems: "center",
             marginTop: "15vh"
         }}>
-            <div class="card shadow-lg" style={{ width: "18rem" }}>
-                <img src={chatroom} class="card-img-top" alt="..." />
-                <h5 class="card-title text-center">JOIN CHAT</h5>
-                <div class="card-body">
-                    <div class="input-group flex-nowrap">
-                        <span class="input-group-text" id="addon-wrapping">@</span>
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"
-                            onChange={(e) => setUserName(e.target.value)} />
+            {!showChat ? (
+                <div class="card shadow-lg" style={{ width: "18rem" }}>
+                    <img src={chatroom} class="card-img-top" alt="..." />
+                    <h5 class="card-title text-center">JOIN CHAT</h5>
+                    <div class="card-body">
+                        <div class="input-group flex-nowrap">
+                            <span class="input-group-text" id="addon-wrapping">@</span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"
+                                onChange={(e) => setUserName(e.target.value)} />
+                        </div>
+
+                        <div class="input-group flex-nowrap mt-2">
+                            <span class="input-group-text" id="addon-wrapping">@</span>
+                            <input type="text" class="form-control" placeholder="Room ID" aria-label="Username" aria-describedby="addon-wrapping"
+                                onChange={(e) => setRoom(e.target.value)} />
+                        </div>
+
+                        <div class="text-center mt-2">
+                            <button type="button" class="btn btn-outline-primary"
+                                onClick={joinRoom}>JOIN ROOM</button>
+                        </div>
                     </div>
-
-                    <div class="input-group flex-nowrap mt-2">
-                        <span class="input-group-text" id="addon-wrapping">@</span>
-                        <input type="text" class="form-control" placeholder="Room ID" aria-label="Username" aria-describedby="addon-wrapping"
-                            onChange={(e) => setRoom(e.target.value)} />
-                    </div>
-
-                    <div class="text-center mt-2">
-                        <button type="button" class="btn btn-outline-primary" 
-                        onClick={joinRoom}>JOIN</button>
-                    </div>
-
-                    <Chats socketProp={socket} username={userName} room={room}/>
-
                 </div>
-            </div>
+            )
+                :
+                (
+                    <Chats socket={socket} username={userName} room={room} />
+                )}
         </div>
     );
 };
