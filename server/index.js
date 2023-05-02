0f8c72a19c5be4dd057b8075a43358cface37524
KingNelx <tapiajonel606@gmail.com>
2023-05-02 22:49:41 +0800

    finish

--- server/index.js
+++ server/index.js
@@ -11,18 +11,25 @@ const server = http.createServer(app);
 const connectionIO = new Server(server,
     {
         cors: {
-            origin: "http://localhost:3000",
+            origin: "http://localhost:5173",
             methods: ["GET", "POST"],
         },
     });
 
-connectionIO.on("connection", (socketIO) => { 
+connectionIO.on("connection", (socketIO) => {
     console.log(`User Connected: ${socketIO.id}`);
+    socketIO.on("JOIN_CHAT_ROOM", (data) => {
+        socketIO.join(data)
+        console.log(`User with ID: ${socketIO.id}, Joined room: ${data} `)
+    })
+    socketIO.on("send_message", (data) => {
+        socketIO.to(data.room).emit("receive_message",data)
+    })
     socketIO.on("disconnect", () => {
         console.log(" USER GOT DISCONNECTED ", socketIO.id)
     });
 });
 
-server.listen(8001, () => {
+server.listen(9000, () => {
     console.log(" SERVER IS UP ")
 })
